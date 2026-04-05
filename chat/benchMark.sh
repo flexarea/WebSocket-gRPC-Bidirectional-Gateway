8b4b77fdf5e344fe19e73830f6b202b61d39c1cf
Esdras Ntuyenabo <esdrasntuyenabo40@gmail.com>
2026-04-05 15:31:24 -0400

    more performance tests

--- chat/benchMark.sh
+++ chat/benchMark.sh
@@ -2,6 +2,6 @@ ghz --insecure \
     --proto ./proto/message.proto \
     --call message.Message/HandleMessage \
     --data '{"content": "hello", "src_user_id": 1, "dest_user_id": 2}' \
-    --concurrency 50 \
-    --total 100000 \
+    --concurrency 10 \
+    --duration 10s \
     localhost:50051
