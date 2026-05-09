9b8a6b64b4201af75845c76c2b666dbfec9b3e7c
Esdras Ntuyenabo <esdrasntuyenabo40@gmail.com>
2026-05-09 14:06:58 -0400

    cmd

--- chat/cmd/gateway/main.go
+++ chat/cmd/gateway/main.go
@@ -1,19 +1,3 @@
-/*
-package main
-
-import (
-	"log"
-	"xchat.io/internal/gateway"
-)
-
-func main() {
-	err := gateway.StartServer(":8080")
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-*/
-
 package main
 
 import (
